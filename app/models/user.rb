d993456f79730d0917c2e3de66b8579756955001
Nyame-Wolf <mumenyam@gmail.com>
2022-11-07 23:22:51 +0300

    fix linter errors after fixing merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-
   has_many :recipe
   has_many :foods
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,4 +94,3 @@ RSpec.configure do |config|
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
 end
-
