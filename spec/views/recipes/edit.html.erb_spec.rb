ad294911f4d1d455a27d26c5508da5052dc42918
Nyame-Wolf <mumenyam@gmail.com>
2022-11-07 23:07:11 +0300

    fix linter errors

--- spec/views/recipes/edit.html.erb_spec.rb
+++ spec/views/recipes/edit.html.erb_spec.rb
@@ -1,31 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "recipes/edit", type: :view do
-  let(:recipe) {
+RSpec.describe 'recipes/edit', type: :view do
+  let(:recipe) do
     Recipe.create!(
-      name: "MyString",
-      description: "MyText",
+      name: 'MyString',
+      description: 'MyText',
       public: false,
       user: nil
     )
-  }
+  end
 
   before(:each) do
     assign(:recipe, recipe)
   end
 
-  it "renders the edit recipe form" do
+  it 'renders the edit recipe form' do
     render
 
-    assert_select "form[action=?][method=?]", recipe_path(recipe), "post" do
+    assert_select 'form[action=?][method=?]', recipe_path(recipe), 'post' do
+      assert_select 'input[name=?]', 'recipe[name]'
 
-      assert_select "input[name=?]", "recipe[name]"
+      assert_select 'textarea[name=?]', 'recipe[description]'
 
-      assert_select "textarea[name=?]", "recipe[description]"
+      assert_select 'input[name=?]', 'recipe[public]'
 
-      assert_select "input[name=?]", "recipe[public]"
-
-      assert_select "input[name=?]", "recipe[user_id]"
+      assert_select 'input[name=?]', 'recipe[user_id]'
     end
   end
 end
