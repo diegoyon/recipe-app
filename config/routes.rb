a7e1fa95df00bf9b2165256c1d731355712818db
Diego Yon <diegoyon@outlook.com>
2022-11-07 13:40:51 -0600

    Display a list of food and added a form that allows user to add new food

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
 end
