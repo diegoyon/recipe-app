c05c4390ad6d81c912f64f307abba02d7874bf73
Nyame-Wolf <mumenyam@gmail.com>
2022-11-07 23:03:45 +0300

    create and make migrations for the recipes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_160227) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_182644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.time "preparation_time"
+    t.time "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_160227) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "recipes", "users"
 end
